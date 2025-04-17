3132dd1bbd3152f69a592adc2ea58f866de5138a
TheScienceDemon <louisruppert0@gmail.com>
2025-04-17 17:21:39 +0200

    Fix Fading conflicting

--- Assets/_Scripts/InterfaceManipulation.cs
+++ Assets/_Scripts/InterfaceManipulation.cs
@@ -2,12 +2,18 @@ using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
 
 public class InterfaceManipulation : MonoBehaviour {
     public static InterfaceManipulation Singleton { get; private set; }
 
     [SerializeField] GameObject countdownDisplay;
     [SerializeField] GameObject engageButton;
+
+    List<DG.Tweening.Core.TweenerCore<Color, Color,
+        DG.Tweening.Plugins.Options.ColorOptions>> currentTweens =
+        new List<DG.Tweening.Core.TweenerCore<Color, Color,
+            DG.Tweening.Plugins.Options.ColorOptions>>();
 
     const float FADE_DURATION = 4f;
 
@@ -16,20 +22,55 @@ public class InterfaceManipulation : MonoBehaviour {
     }
 
     public void HideEngageButton() {
-        var tween = engageButton.GetComponent<Image>().DOFade(0f, FADE_DURATION);
-        engageButton.GetComponentInChildren<TMP_Text>().DOFade(0f, FADE_DURATION);
+        KillCurrentTweens();
 
-        tween.OnComplete(() => {
-            countdownDisplay.GetComponent<TMP_Text>().DOFade(1f, FADE_DURATION);
+        var tween1 = engageButton.GetComponent<Image>()
+            .DOFade(0f, FADE_DURATION);
+
+        var tween2 = engageButton.GetComponentInChildren<TMP_Text>()
+            .DOFade(0f, FADE_DURATION);
+
+        currentTweens.Add(tween1);
+        currentTweens.Add(tween2);
+
+        tween1.OnComplete(() => {
+            currentTweens.Remove(tween1);
+            currentTweens.Remove(tween2);
+
+            var tween3 = countdownDisplay.GetComponent<TMP_Text>()
+                .DOFade(1f, FADE_DURATION);
+
+            currentTweens.Add(tween3);
         });
     }
 
     public void ShowEngageButton() {
-        var tween = countdownDisplay.GetComponent<TMP_Text>().DOFade(0f, FADE_DURATION);
+        KillCurrentTweens();
 
-        tween.OnComplete(() => {
-            engageButton.GetComponent<Image>().DOFade(1f, FADE_DURATION);
-            engageButton.GetComponentInChildren<TMP_Text>().DOFade(1f, FADE_DURATION);
+        var tween1 = countdownDisplay.GetComponent<TMP_Text>()
+            .DOFade(0f, FADE_DURATION);
+
+        currentTweens.Add(tween1);
+
+        tween1.OnComplete(() => {
+            currentTweens.Remove(tween1);
+
+            var tween2 = engageButton.GetComponent<Image>()
+                .DOFade(1f, FADE_DURATION);
+
+            var tween3 = engageButton.GetComponentInChildren<TMP_Text>()
+                .DOFade(1f, FADE_DURATION);
+
+            currentTweens.Add(tween1);
+            currentTweens.Add(tween2);
         });
+    }
+
+    void KillCurrentTweens() {
+        foreach (var tween in currentTweens) {
+            tween.Kill();
+        }
+
+        currentTweens.Clear();
     }
 }
